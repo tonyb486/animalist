d78258e2414d38ed6cc44c1a5053bb1b997e4b5a
Vivian Rose <26861054+Roachbones@users.noreply.github.com>
2026-01-06 20:07:01 -0600

    Thank

---
+++ thanks.js
@@ -0,0 +1,13 @@
+THANKS=[
+    "<a href=https://mou.sh target=_blank>Olive</a> for playtesting",
+    "<a href=https://devin.gay target=_blank>Devin</a> for early playtesting",
+    "<a href=https://novalinium.com target=_blank>Nova</a> for appreciation and encouragement",
+    "Denizens of the Book of Creation Undone",
+    "Lynn Hughes for data advice",
+    "<a href=https://electr1.ca target=_blank>Teri</a> for early playtesting",
+    "<a href=https://soapie.space target=_blank>Sophie</a> et al for early playtesting",
+    "Ada for early playtesting",
+    "<i>simple place</i> friends",
+    "Elle for taking care of me while I was sick",
+    "<a href=https://kateshrike.net target=_blank>Kate</a> for inspiring me by <a href=https://mastodon.social/@kate@shrike.club/115645496671627096 target=_blank>phrasing the title as a command</a>"
+]
